82c210b213fb3be7cc9027f676132178b6cb7633
Antoniopjunior <mr.apjunior@gmail.com>
2023-02-14 19:00:18 -0300

    aula 2

---
+++ 01.py
@@ -0,0 +1,10 @@
+# 2
+s_km=input("Qual a distância?")
+s_h=input("Quanto tempo?")
+km=int(s_km)
+h=int(s_h)
+velocidade=km/h
+print("Velocidade:", velocidade)
+print(type(s_km))
+print(type(km))
+print(type(velocidade))
